8fe65f24eeca5d0cb948300d2d0fe65d7528ff60
yuriyriznyk <yuriyriznyk@users.noreply.github.com>
2017-11-23 02:06:34 +0200

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 #
-#Purpose: build images consists nginx 
+#Purpose: build images consists nginx-lua
 
 docker build -t 'local/nginx:local' --no-cache --force-rm .
 #docker tag local/nginx:local docker-repo.pitchbook.com/riznykyurii-nginx:$(date +%Y%m%d)
